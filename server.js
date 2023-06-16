e466ccd3174e2b7ea6fa30177bd0e17b157a17e2
sethengland <44851807+sethengland@users.noreply.github.com>
2023-06-16 16:29:38 -0700

    fix port issue

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ app.use(express.static('public'));
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
-const port = 3000;
+const port = process.env.PORT || 3000;;
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
